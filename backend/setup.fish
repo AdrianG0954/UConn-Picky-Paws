953aade7928406f62a8f14a38141977c270a83d3
Vladislav Vetroff <vvvetroff@niobe.hinny-delta.ts.net>
2026-03-28 22:29:50 -0400

    chore: updated setup shell script; included fish script

--- backend/setup.fish
+++ backend/setup.fish
@@ -1,13 +1,13 @@
-#!/bin/bash
-# RUN THIS COMMAND WITH source ./setup.sh
+#!/usr/bin/env fish
+# RUN THIS COMMAND WITH source ./setup.fish
 # If not using source, it will be unable to use any source commands on your current shell environment
 
 # create virtual environment
 python3 -m venv .venv
 
 # activate virtual environment
-source .venv/bin/activate
+source .venv/bin/activate.fish
 
 # install dependencies
-pip3 install -r requirements.txt
-
+pip install --upgrade pip
+pip install -r requirements.txt
