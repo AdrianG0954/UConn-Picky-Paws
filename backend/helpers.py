3bfe093317e2b0a57801b0e4f5a587fd7fa05145
Vladislav Vetroff <vladislav.vetroff@gmail.com>
2026-03-29 02:23:44 -0400

    feat: getting two random meals from db

---
+++ backend/helpers.py
@@ -0,0 +1,36 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, func
+from database.food import Food
+from pydantic import BaseModel
+
+class DishInfo(BaseModel):
+	name: str
+	nutrition_info: dict
+	elo_rating: float
+
+class RandomMealsResponse(BaseModel):
+	meals: list[DishInfo]
+
+
+async def get_random_meals_from_db(db_session: AsyncSession) -> RandomMealsResponse:
+	"""
+	Gets a random meal from the database.
+	"""
+	stmt = select(Food).where(Food.elo_rating.isnot(None)).order_by(func.random()).limit(2)
+
+	res = await db_session.execute(stmt)
+	entries = res.scalars().all()
+	if len(entries) != 2:
+		raise ValueError("Failed to get 2 random meals.")
+
+	return RandomMealsResponse(
+		meals=[
+			DishInfo(
+				name=entry.name,
+				nutrition_info=entry.nutrition_info,
+				elo_rating=entry.elo_rating
+			) for entry in entries
+		]
+	)
+
+	
