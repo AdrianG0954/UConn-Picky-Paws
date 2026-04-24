4ba482dcb5523c49a24ca26c8402c9279c8b7651
Adrian Garcia <adrianrgarcia010@gmail.com>
2026-04-23 20:29:02 -0400

    pleaseee

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     port: 3000,
   },
   preview: {
+    allowedHosts: ["frontend-production-d580.up.railway.app"],
     host: true,
     port: Number.parseInt(process.env.PORT ?? "3000", 10),
     strictPort: true,
