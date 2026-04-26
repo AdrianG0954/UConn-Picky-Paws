283fdb8e9ac28ca36df573a9c5409ae47b84d968
jobegets <jobe.goetz@uconn.edu>
2026-04-25 20:07:14 -0400

    add email to footer

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -54,6 +54,12 @@ export function Layout() {
         <p className="mx-auto max-w-3xl text-sm leading-relaxed text-zinc-600">
           All data is sourced from the University's dining services website.
         </p>
+        <p className="mx-auto mt-3 max-w-3xl text-sm leading-relaxed text-zinc-600">
+          For any feedback or bugs, contact:{" "}
+          <span className="mx-auto max-w-3xl text-sm leading-relaxed text-uconn-navy">
+            pickypawsuconn@gmail.com
+          </span>
+        </p>
       </footer>
     </div>
   );
